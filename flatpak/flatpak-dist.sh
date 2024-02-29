adab1d63bd39e6822532467c457dd2ccff6981be
Tabinol <8842894+Tabinol@users.noreply.github.com>
2024-02-28 21:00:51 -0500

    Fix change dist to release and disable upload

--- flatpak/flatpak-dist.sh
+++ flatpak/flatpak-dist.sh
@@ -6,5 +6,5 @@ PACKAGE_VERSION=`jq --raw-output '.version' ../package.json`
 
 flatpak-node-generator yarn ../yarn.lock
 flatpak-builder build me.tabinol.fastmail-desktop-client.yml --repo=repo --force-clean
-mkdir -p ../dist
-flatpak build-bundle repo ../dist/fastmail-desktop-client-${PACKAGE_VERSION}.flatpak me.tabinol.fastmail-desktop-client
+mkdir -p ../release
+flatpak build-bundle repo ../release/fastmail-desktop-client-${PACKAGE_VERSION}.flatpak me.tabinol.fastmail-desktop-client
